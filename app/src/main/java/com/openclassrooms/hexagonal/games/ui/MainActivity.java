a552dd86d6923aa45d75840d5a4bcf7004406fe0
Ludovic Roland <ludovic.c.roland@gmail.com>
2024-04-02 23:33:47 +0200

    chore: Clean-up the code

--- app/src/main/java/com/openclassrooms/hexagonal/games/ui/MainActivity.java
+++ app/src/main/java/com/openclassrooms/hexagonal/games/ui/MainActivity.java
@@ -1,9 +1,6 @@
 package com.openclassrooms.hexagonal.games.ui;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -11,7 +8,6 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.openclassrooms.hexagonal.games.R;
 import com.openclassrooms.hexagonal.games.databinding.ActivityMainBinding;
 import dagger.hilt.android.AndroidEntryPoint;
@@ -38,52 +34,20 @@ public final class MainActivity
 
     setSupportActionBar(binding.toolbar);
 
-    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+    final NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
     appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
     NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-    binding.fab.setOnClickListener(new View.OnClickListener()
-    {
-      @Override
-      public void onClick(View view)
-      {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAnchorView(R.id.fab)
-            .setAction("Action", null).show();
-      }
+    binding.fab.setOnClickListener(view -> {
+      //TODO
     });
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu)
-  {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item)
-  {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings)
-    {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
   }
 
   @Override
   public boolean onSupportNavigateUp()
   {
-    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-    return NavigationUI.navigateUp(navController, appBarConfiguration)
-        || super.onSupportNavigateUp();
+    final NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+    return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
   }
+
 }
