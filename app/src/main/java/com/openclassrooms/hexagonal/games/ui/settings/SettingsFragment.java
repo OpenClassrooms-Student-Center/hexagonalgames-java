47b294c2dd576f159be63fd78bf1476583635517
Ludovic Roland <ludovic.c.roland@gmail.com>
2024-04-04 22:33:27 +0200

    feature(settings): finish the MVP of the settings screen

--- app/src/main/java/com/openclassrooms/hexagonal/games/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/openclassrooms/hexagonal/games/ui/settings/SettingsFragment.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.openclassrooms.hexagonal.games.databinding.FragmentSettingsBinding;
 import dagger.hilt.android.AndroidEntryPoint;
@@ -18,22 +19,38 @@ public final class SettingsFragment
 
   private FragmentSettingsBinding binding;
 
-  @Override
-  public View onCreateView(
-      @NonNull LayoutInflater inflater, ViewGroup container,
-      Bundle savedInstanceState
-  )
-  {
+  private SettingsViewModel viewModel;
 
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState)
+  {
     binding = FragmentSettingsBinding.inflate(inflater, container, false);
     return binding.getRoot();
-
   }
 
   public void onViewCreated(@NonNull View view, Bundle savedInstanceState)
   {
     super.onViewCreated(view, savedInstanceState);
 
+    setupUI();
+    setupViewModel();
+  }
+
+  private void setupViewModel()
+  {
+    viewModel = new ViewModelProvider(this).get(SettingsViewModel.class);
+  }
+
+  private void setupUI()
+  {
+    binding.notificationEnable.setOnClickListener(v -> {
+      viewModel.enableNotifications();
+    });
+
+    binding.notificationDisable.setOnClickListener(v -> {
+      viewModel.disableNotifications();
+    });
   }
 
   @Override
---
+++ app/src/main/java/com/openclassrooms/hexagonal/games/ui/settings/SettingsViewModel.java
@@ -0,0 +1,19 @@
+package com.openclassrooms.hexagonal.games.ui.settings;
+
+import androidx.lifecycle.ViewModel;
+
+public final class SettingsViewModel
+    extends ViewModel
+{
+
+  public void enableNotifications()
+  {
+    //TODO
+  }
+
+  public void disableNotifications()
+  {
+    //TODO
+  }
+
+}
