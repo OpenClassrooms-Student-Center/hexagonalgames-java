bd5f143d9704553e1639dde3d21621f40fc3c37a
ludovicroland <ludovic.roland@decathlon.com>
2024-04-08 17:27:07 +0200

    feat(settings): Add the management of the notification permission

--- app/src/main/java/com/openclassrooms/hexagonal/games/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/openclassrooms/hexagonal/games/ui/settings/SettingsFragment.java
@@ -1,15 +1,21 @@
 package com.openclassrooms.hexagonal.games.ui.settings;
 
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.openclassrooms.hexagonal.games.databinding.FragmentSettingsBinding;
+
 import dagger.hilt.android.AndroidEntryPoint;
 
 /**
@@ -17,63 +23,70 @@ import dagger.hilt.android.AndroidEntryPoint;
  */
 @AndroidEntryPoint
 public final class SettingsFragment
-    extends Fragment
-{
+        extends Fragment {
 
-  /**
-   * View binding object for the fragment's layout (fragment_settings.xml).
-   */
-  private FragmentSettingsBinding binding;
+    /**
+     * View binding object for the fragment's layout (fragment_settings.xml).
+     */
+    private FragmentSettingsBinding binding;
 
-  /**
-   * ViewModel responsible for handling data and events related to application settings.
-   */
-  private SettingsViewModel viewModel;
+    /**
+     * ViewModel responsible for handling data and events related to application settings.
+     */
+    private SettingsViewModel viewModel;
 
-  @Override
-  public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-      Bundle savedInstanceState)
-  {
-    binding = FragmentSettingsBinding.inflate(inflater, container, false);
-    return binding.getRoot();
-  }
-
-  @Override
-  public void onViewCreated(@NonNull View view, Bundle savedInstanceState)
-  {
-    super.onViewCreated(view, savedInstanceState);
-
-    setupUI();
-    setupViewModel();
-  }
-
-  /**
-   * Retrieves the SettingsViewModel instance for this fragment.
-   */
-  private void setupViewModel()
-  {
-    viewModel = new ViewModelProvider(this).get(SettingsViewModel.class);
-  }
-
-  /**
-   * Initializes UI elements and sets up click listeners for the notification toggle buttons.
-   */
-  private void setupUI()
-  {
-    binding.notificationEnable.setOnClickListener(v -> {
-      viewModel.enableNotifications();
+    /**
+     * Launcher for requesting the notification permission from the user.
+     */
+    private ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+        //we do not check for the result here
     });
 
-    binding.notificationDisable.setOnClickListener(v -> {
-      viewModel.disableNotifications();
-    });
-  }
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
 
-  @Override
-  public void onDestroyView()
-  {
-    super.onDestroyView();
-    binding = null;
-  }
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        setupUI();
+        setupViewModel();
+    }
+
+    /**
+     * Retrieves the SettingsViewModel instance for this fragment.
+     */
+    private void setupViewModel() {
+        viewModel = new ViewModelProvider(this).get(SettingsViewModel.class);
+    }
+
+    /**
+     * Initializes UI elements and sets up click listeners for the notification toggle buttons.
+     */
+    private void setupUI() {
+        binding.notificationEnable.setOnClickListener(v -> {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_DENIED) {
+                    requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS);
+                }
+            }
+
+            viewModel.enableNotifications();
+        });
+
+        binding.notificationDisable.setOnClickListener(v -> {
+            viewModel.disableNotifications();
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 
 }
